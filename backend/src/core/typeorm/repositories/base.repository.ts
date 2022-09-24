9439cad55098b3d21291d22eb9051f9937255249
choewy <choewy32@gmail.com>
2022-09-24 15:01:09 +0900

    refactor repositories

--- backend/src/core/typeorm/repositories/base.repository.ts
+++ backend/src/core/typeorm/repositories/base.repository.ts
@@ -1,14 +1,28 @@
+import { Injectable } from '@nestjs/common';
 import { DataSource } from 'typeorm';
-import { Role, User } from '../entities';
+import { Role, RolePolicy, User } from '../entities';
 
+@Injectable()
 export class BaseRepository {
   constructor(protected dataSource: DataSource) {}
 
-  get user() {
+  protected get role() {
+    return this.dataSource.getRepository(Role);
+  }
+
+  protected get Role() {
+    return Role;
+  }
+
+  protected get RolePolicy() {
+    return RolePolicy;
+  }
+
+  protected get user() {
     return this.dataSource.getRepository(User);
   }
 
-  get role() {
-    return this.dataSource.getRepository(Role);
+  protected get User() {
+    return User;
   }
 }
--- backend/src/core/typeorm/repositories/role.repository.ts
+++ backend/src/core/typeorm/repositories/role.repository.ts
@@ -1,14 +1,9 @@
+import { Role } from '../entities';
 import { Injectable } from '@nestjs/common';
-import { DataSource } from 'typeorm';
-import { Role, RolePolicy } from '../entities';
 import { BaseRepository } from './base.repository';
 
 @Injectable()
 export class RoleRepository extends BaseRepository {
-  constructor(protected dataSource: DataSource) {
-    super(dataSource);
-  }
-
   async findDefaultRole(): Promise<Role> {
     return await this.role.findOne({
       relations: { rolePolicy: true },
@@ -32,37 +27,23 @@ export class RoleRepository extends BaseRepository {
   }
 
   async insertDefaultRole(): Promise<void> {
-    const role = await this.role.save(
-      Object.assign<Role, Partial<Role>>(new Role(), {
-        name: '역할없음',
-      }),
-    );
+    const role = new this.Role();
 
-    role.rolePolicy = Object.assign<RolePolicy, Partial<RolePolicy>>(
-      new RolePolicy(),
-      {
-        roleId: role.id,
-        default: true,
-      },
-    );
+    role.name = '역할없음';
+    role.rolePolicy = new this.RolePolicy();
+    role.rolePolicy.roleId = 1;
+    role.rolePolicy.default = true;
 
     await this.role.save(role);
   }
 
   async insertMasterRole(): Promise<void> {
-    const role = await this.role.save(
-      Object.assign<Role, Partial<Role>>(new Role(), {
-        name: '마스터',
-      }),
-    );
+    const role = new this.Role();
 
-    role.rolePolicy = Object.assign<RolePolicy, Partial<RolePolicy>>(
-      new RolePolicy(),
-      {
-        roleId: role.id,
-        master: true,
-      },
-    );
+    role.name = '마스터';
+    role.rolePolicy = new this.RolePolicy();
+    role.rolePolicy.roleId = 2;
+    role.rolePolicy.master = true;
 
     await this.role.save(role);
   }
