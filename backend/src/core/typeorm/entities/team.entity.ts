a0d89402c8f0d8d739a6efe7844919fed642c7e1
choewy <choewy32@gmail.com>
2022-09-24 16:51:03 +0900

    feature team entity & repository

---
+++ backend/src/core/typeorm/entities/team.entity.ts
@@ -0,0 +1,17 @@
+import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { DateTimeEntity } from './datetime.entity';
+import { User } from './user.entity';
+
+class Relation extends DateTimeEntity {
+  @ManyToMany(() => User, (e) => e.teams)
+  users: User[];
+}
+
+@Entity('team')
+export class Team extends Relation {
+  @PrimaryGeneratedColumn()
+  readonly id: number;
+
+  @Column()
+  name: string;
+}
