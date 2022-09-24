f45f4be4642f2ddbfb91e514366f7921b2892c49
choewy <choewy32@gmail.com>
2022-09-24 21:22:34 +0900

    feature datetime to format transformer

---
+++ backend/src/appllication/transformer/datetime.transformer.ts
@@ -0,0 +1,15 @@
+import { Transform } from 'class-transformer';
+import { DateTime } from 'luxon';
+
+export const DateTimetoformat = (format?: string) =>
+  Transform(({ value }) => {
+    if (DateTime.isDateTime(value)) {
+      if (format) {
+        return value.toFormat(format);
+      } else {
+        return value.toFormat('yyyy-MM-dd hh:mm:ss');
+      }
+    }
+
+    return null;
+  });
