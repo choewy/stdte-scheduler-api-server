0d43284b528a46be41483e17b481dcb5014d5756
choewy <choewy32@gmail.com>
2022-09-26 18:21:37 +0900

    refactor app

--- frontend/src/app/app.tsx
+++ frontend/src/app/app.tsx
@@ -1,12 +1,13 @@
+import { FC } from 'react';
 import { ROUTER } from '@/configs';
 import { LoginPage } from '@/pages';
-import { FC } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { useAppAuthCheck, useAppAuthGuard } from './app.hook';
 
 export const App: FC = () => {
   useAppAuthCheck();
   useAppAuthGuard();
+
   return (
     <Routes>
       <Route path={ROUTER.signin} element={<LoginPage />} />
