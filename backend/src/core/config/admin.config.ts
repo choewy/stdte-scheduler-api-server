f7eef157cdd2c4ecb7eacf8ef7af5c7d1b007d1b
choewy <choewy32@gmail.com>
2022-09-29 00:13:18 +0900

    refactor: backend - master, admin config

--- backend/src/core/config/admin.config.ts
+++ backend/src/core/config/admin.config.ts
@@ -1,12 +1,8 @@
 import { registerAs } from '@nestjs/config';
-import { hashPassword } from '../bcrypt';
 import { ConfigToken, EnvKey } from './enums';
 import { getEnvWithPrefix } from './helpers';
 import { UserConfig } from './interfaces';
 
 export default registerAs(ConfigToken.Admin, (): UserConfig => {
-  const config = getEnvWithPrefix<UserConfig>(EnvKey.ADMIN);
-  return Object.assign(config, {
-    password: hashPassword(config.password),
-  });
+  return getEnvWithPrefix<UserConfig>(EnvKey.ADMIN);
 });
--- backend/src/core/config/master.config.ts
+++ backend/src/core/config/master.config.ts
@@ -1,12 +1,8 @@
 import { registerAs } from '@nestjs/config';
-import { hashPassword } from '../bcrypt';
 import { ConfigToken, EnvKey } from './enums';
 import { getEnvWithPrefix } from './helpers';
 import { UserConfig } from './interfaces';
 
 export default registerAs(ConfigToken.Master, (): UserConfig => {
-  const config = getEnvWithPrefix<UserConfig>(EnvKey.MASTER);
-  return Object.assign(config, {
-    password: hashPassword(config.password),
-  });
+  return getEnvWithPrefix<UserConfig>(EnvKey.MASTER);
 });
