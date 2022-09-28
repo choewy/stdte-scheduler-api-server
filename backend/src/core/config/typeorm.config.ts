00442081b6f31e919647d62335e8ad03286d8258
choewy <choewy32@gmail.com>
2022-09-28 17:38:55 +0900

    fix: backend - typeorm luxon type load

--- backend/src/core/config/typeorm.config.ts
+++ backend/src/core/config/typeorm.config.ts
@@ -4,6 +4,7 @@ import { ConfigToken, EnvKey } from './enums';
 import { getEnvWithPrefix } from './helpers';
 import { TypeormEnv } from './interfaces';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import { DateTime } from 'luxon';
 
 export default registerAs(ConfigToken.Typeorm, (): TypeOrmModuleOptions => {
   const env = getEnvWithPrefix<TypeormEnv>(EnvKey.TYPEORM);
@@ -19,6 +20,35 @@ export default registerAs(ConfigToken.Typeorm, (): TypeOrmModuleOptions => {
     entities: env.entities.split(', '),
     migrations: env.migrations.split(', '),
     timezone: env.timezone,
+    autoLoadEntities: true,
     namingStrategy: new SnakeNamingStrategy(),
+    extra: {
+      typeCast: (field: any, next: () => void) => {
+        let val: string;
+
+        switch (field.type) {
+          case 'DATE':
+            val = field.string();
+            return val === null
+              ? null
+              : DateTime.fromFormat(
+                  val.length > 10 ? val.slice(0, 10) : val,
+                  'yyyy-MM-dd',
+                );
+
+          case 'DATETIME':
+            val = field.string();
+            return val === null
+              ? null
+              : DateTime.fromFormat(
+                  val.length > 19 ? val.slice(0, 19) : val,
+                  'yyyy-MM-dd HH:mm:ss',
+                );
+
+          default:
+            return next();
+        }
+      },
+    },
   };
 });
