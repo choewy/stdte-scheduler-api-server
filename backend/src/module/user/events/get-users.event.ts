592eeff1317c63a5c3271824ad226cd5553ab431
choewy <choewy32@gmail.com>
2022-09-29 17:25:17 +0900

    fix: backend - exclude role types(manager -> master)

--- backend/src/module/user/events/get-users.event.ts
+++ backend/src/module/user/events/get-users.event.ts
@@ -2,10 +2,11 @@ import { RoleType } from '@/core/typeorm/entities';
 import { UserRowDto } from '../dto';
 import { UserRepository } from '../user.repository';
 
+const EXCLUDE_ROLE_TYPES = [RoleType.Master, RoleType.Admin];
+
 export const getUsersEvent = async (
   repository: UserRepository,
-  types?: RoleType[],
 ): Promise<UserRowDto[]> => {
-  const users = await repository.findUsers(types);
+  const users = await repository.findUsers(EXCLUDE_ROLE_TYPES);
   return users.map((user) => new UserRowDto(user));
 };
