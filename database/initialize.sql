b6761ce9c6a55ca5cba1b8eb0cf3b03f2434dae7
choewy <choewy32@gmail.com>
2022-10-01 23:56:21 +0900

    fix: database - docker compose

--- database/initialize.sql
+++ database/initialize.sql
@@ -1,8 +1,6 @@
 SET GLOBAL time_zone="Asia/Seoul";
 SET time_zone="Asia/Seoul";
 
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
-
 CREATE DATABASE IF NOT EXISTS stdte_task_local CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 CREATE DATABASE IF NOT EXISTS stdte_task_test CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 CREATE DATABASE IF NOT EXISTS stdte_task_develop CHARACTER SET utf8 COLLATE utf8_unicode_ci;
