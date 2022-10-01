892348415f99fe81f0a2cdf0c0d36c42c34a759c
choewy <choewy32@gmail.com>
2022-10-01 21:47:59 +0900

    edit: database - initialize sql query

--- database/initialize.sql
+++ database/initialize.sql
@@ -1,6 +1,8 @@
 SET GLOBAL time_zone="Asia/Seoul";
 SET time_zone="Asia/Seoul";
 
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
+
 CREATE DATABASE IF NOT EXISTS stdte_task_local CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 CREATE DATABASE IF NOT EXISTS stdte_task_test CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 CREATE DATABASE IF NOT EXISTS stdte_task_develop CHARACTER SET utf8 COLLATE utf8_unicode_ci;
