497fdf055e8bd4062206af419af21f811ba16508
choewy <choewy32@gmail.com>
2022-10-26 12:16:43 +0900

    feature: backend - init team module

---
+++ backend/src/modules/team/enums.ts
@@ -0,0 +1,12 @@
+export enum TeamMessage {
+  All = 'team:all',
+  Create = 'team:create',
+  Update = 'team:create',
+  Delete = 'team:delete',
+  SyncCreate = 'team:create:sync',
+  SyncUpdate = 'team:update:sync',
+  SyncDelete = 'team:delete:sync',
+  SearchMember = 'team:member:search',
+  AppendMember = 'team:member:append',
+  RemoveMember = 'team:member:remove',
+}
